5167615478d9122e2cafb0634bfb9de5233bf5ae
Constantin Piber <constantin.piber@student.tugraz.at>
2020-01-16 12:13:57 +0100

    Updates

---
+++ precache-manifest.d2635cfded813c0ac5400c1f3fca9f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd869c10348ed62b24101575f2f6c4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a37a080ca35c1bbcedae",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05747d090501e00b6613",
+    "url": "./static/js/2.584e98f2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.584e98f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a37a080ca35c1bbcedae",
+    "url": "./static/js/main.fd828c21.chunk.js"
+  },
+  {
+    "revision": "c3b3cfabf7c02dd9cc79",
+    "url": "./static/js/runtime-main.773fc470.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
